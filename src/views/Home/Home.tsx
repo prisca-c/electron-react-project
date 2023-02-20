2b0e6db7fe70fff8dd44c9ec053a1e1ca1afc469
Prisca C <prisca.castanier@gmail.com>
2023-02-20 11:56:22 +0100

    remove unused import, functions and datas

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,19 +1,7 @@
-import {useSelector} from 'react-redux';
-
 const Home = () => {
-  const cart = useSelector((state: any) => state.cart);
-
   return (
     <div>
       <h1>Home</h1>
-
-      <div>
-        <p>
-          Cart Items Count: <b>{cart.itemsCount}</b>
-          <br/> Cart Total: <b>{cart.value}</b> $
-        </p>
-
-      </div>
     </div>
   );
 };
