04174dfdb399e55e264ec87e042fa814e98bc2da
Prisca C <prisca.castanier@gmail.com>
2023-02-23 21:12:44 +0100

    fix: fix issue with state while unit testing

--- src/renderer/services/__tests__/App.test.tsx
+++ src/renderer/services/__tests__/App.test.tsx
@@ -1,9 +1,12 @@
+import { act } from 'react-test-renderer';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../../components/App';
 
 describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
+  it('should render', async () => {
+    await act(async () => {
+      expect(render(<App />)).toBeTruthy();
+    });
   });
 });
