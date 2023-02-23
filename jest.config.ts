e151bd3f65a716221d9c5171bac542c879be2ef3
Prisca C <prisca.castanier@gmail.com>
2023-02-23 21:11:05 +0100

    create jest config file to separate jest config from package.json

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  moduleDirectories: ['node_modules', 'release/app/node_modules', 'src'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/.erb/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFiles: ['./.erb/scripts/check-build-exists.ts'],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost/',
+  },
+  testPathIgnorePatterns: ['release/app/dist', '.erb/dll'],
+  transform: {
+    '\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom', './jest.setup.ts'],
+};
+
+export default config;
