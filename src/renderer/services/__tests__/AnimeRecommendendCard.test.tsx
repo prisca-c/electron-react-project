d994557c99ce316024c0a610b673d5b2940df08e
Prisca C <prisca.castanier@gmail.com>
2023-02-23 21:33:20 +0100

    test: create UT on button click

--- src/renderer/services/__tests__/AnimeRecommendendCard.test.tsx
+++ src/renderer/services/__tests__/AnimeRecommendendCard.test.tsx
@@ -1,7 +1,8 @@
+import { act } from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import { AnimeRecommendCard } from '../../components/animeRecommendations/AnimeRecommendCard';
 import { nullAnimeRecommendation } from '../../types/AnimeRecommendedType';
-import { TestLayout } from './TestLayout';
+import { UnitTestingLayout } from '../../layouts/UnitTestingLayout';
 
 describe('AnimeRecommendCard', () => {
   test('should render successfully', async () => {
@@ -12,10 +13,31 @@ describe('AnimeRecommendCard', () => {
     };
 
     const { baseElement } = render(
-      <TestLayout>
+      <UnitTestingLayout>
         <AnimeRecommendCard {...props} />
-      </TestLayout>
+      </UnitTestingLayout>
     );
     expect(baseElement).toBeTruthy();
+  });
+
+  test('it should trigger the addToWatchList function', async () => {
+    const recommendations = [nullAnimeRecommendation];
+
+    const props = {
+      data: recommendations,
+    };
+
+    const { getByText } = render(
+      <UnitTestingLayout>
+        <AnimeRecommendCard {...props} />
+      </UnitTestingLayout>
+    );
+    const button = getByText('💟');
+
+    button.click();
+
+    await act(async () => {
+      expect(button).toBeTruthy();
+    });
   });
 });
