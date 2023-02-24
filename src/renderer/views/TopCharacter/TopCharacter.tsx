1d2a12c059733e1d65a770095d55409f754fd7eb
Prisca C <prisca.castanier@gmail.com>
2023-02-24 17:23:36 +0100

    style change

--- src/renderer/views/TopCharacter/TopCharacter.tsx
+++ src/renderer/views/TopCharacter/TopCharacter.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import { useAppSelector } from '../../hooks/useAppSelector';
+import { useAppSelector, useAppDispatch } from '../../hooks/useAppSelector';
 import { selectCharacters } from '../../store/selectors/charactersSelector';
-import { useAppDispatch } from '../../hooks/useAppSelector';
 import { fetchCharacters } from '../../store/effetcs/charactersFecthEffect';
+import Styles from './TopCharacter.module.scss';
 
 const TopCharacter = () => {
   const topCharacters = useAppSelector(selectCharacters);
@@ -16,13 +16,15 @@ const TopCharacter = () => {
   return (
     <div>
       <h1>Top Character</h1>
-      {topCharacters.info.length > 0 &&
-        topCharacters.info.map((character) => (
-          <div key={character.mal_id}>
-            <h2>{character.name}</h2>
-            <img src={character.images.jpg.image_url} alt={character.name} />
-          </div>
-        ))}
+      <div className={Styles.container}>
+        {topCharacters.info.length > 0 &&
+          topCharacters.info.map((character) => (
+            <div key={character.mal_id}>
+              <h2>{character.name}</h2>
+              <img src={character.images.jpg.image_url} alt={character.name} />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
